97fff36a0c9dcbee3cddf3f590827230bff04ef4
Rizer <k4hawk@gmail.com>
2018-07-25 10:37:45 +0300

    Update Functions.php

--- Functions.php
+++ Functions.php
@@ -13,34 +13,16 @@ sleep(1);
 $str=shell_exec("strings $name");
 
 //888 rat
-if(preg_match("/6595b64144ccf1df/",$str))
-{
-if(preg_match("/PSAPI.DLL/",$str))
-{
-if(preg_match("/WNetGetConnectionW/",$str))
-{
+if(preg_match("/6595b64144ccf1df/",$str)&&preg_match("/PSAPI.DLL/",$str)&&preg_match("/WNetGetConnectionW/",$str)
+   {
 echo $red."[+] Found 888 rat \n";
 $risk = $risk + 10;
 }
-else
-{
-echo $green."[-] Clear no 888 rat \n";
-}
-}
-else
-{
-echo $green."[-] Clear no 888 rat \n";
-}
-}
-else
-{
 echo $green."[-] Clear no 888 rat \n";
 }
 
 //DarkTrack Rat 4.1
-if(preg_match("/2005 Pierre le Riche/",$str))
-{
-if(preg_match("/4.1 Alien+/",$str))
+if(preg_match("/2005 Pierre le Riche/",$str)&&preg_match("/4.1 Alien+/",$str))
 {
 echo $red."[+] Found DarkTrack Rat 4.1 \n";
 $risk = $risk + 10;
@@ -49,16 +31,9 @@ else
 {
 echo $green."[-] Clear no DarkTrack Rat 4.1 \n";
 }
-}
-else
-{
-echo $green."[-] Clear no DarkTrack Rat 4.1 \n";
-}
 
 //njrat
-if(preg_match("/j.exe/",$str))
-{
-if(preg_match("/NtSetInformationProcess/",$str))
+if(preg_match("/j.exe/",$str)&&preg_match("/NtSetInformationProcess/",$str))
 {
 echo $red."[+] Found njRat \n";
 $risk = $risk + 10;
@@ -67,16 +42,9 @@ else
 {
 echo $green."[-] Clear no njRat \n";
 }
-}
-else
-{
-echo $green."[-] Clear no njRat \n";
-}
 
 //XtermeRat
-if(preg_match("/7kOIsWow64/",$str))
-{
-if(preg_match("/Snapsho$H/",$str))
+if(preg_match("/7kOIsWow64/",$str)&&preg_match('/Snapsho$H/',$str))
 {
 echo $red."[+] Found XtermeRat \n";
 $risk = $risk + 10;
@@ -85,49 +53,22 @@ else
 {
 echo $green."[-] Clear no XtermeRat \n";
 }
-}
-else
-{
-echo $green."[-] Clear no XtermeRat \n";
-}
 
 //CyberGate Rat
-if(preg_match("/gUnitServerUtils/",$str))
-{
-if(preg_match("/wcrypt2/",$str))
-{
-if(preg_match("/KuURLHistory/",$str))
+if(preg_match("/gUnitServerUtils/",$str)&&preg_match("/wcrypt2/",$str)&&preg_match("/KuURLHistory/",$str))
 {
 echo $red."[+] Found CyberGate Rat \n";
 $risk = $risk + 10;
 }
-else
-{
-echo $green."[-] Clear no CyberGate Rat \n";
-}
-}
-else
-{
-echo $green."[-] Clear no CyberGate Rat \n";
-}
-}
-else
-{
+else{
 echo $green."[-] Clear no CyberGate Rat \n";
 }
 
 //Quasar Rat 1.3.0.0
-if(preg_match("/xClient.Core.Packets.ServerPackets/",$str))
-{
-if(preg_match("/<RemotePort>k__BackingField/",$str))
+if(preg_match("/xClient.Core.Packets.ServerPackets/",$str)&&preg_match("/<RemotePort>k__BackingField/",$str))
 {
 echo $red."[+] Found Quasar Rat 1.3.0.0\n";
 $risk = $risk + 10;
-}
-else
-{
-echo $green."[-] Clear no Quasar Rat 1.3.0.0\n";
-}
 }
 else
 {
@@ -146,17 +87,10 @@ echo $green."[-] Clear no VanTomRat 1.4 \n";
 }
 
 //Beast Trojan Horse
-if(preg_match("/2003 Avenger b/",$str))
-{
-if(preg_match("/42CE4021-DE0/",$str))
+if(preg_match("/2003 Avenger b/",$str)&&preg_match("/42CE4021-DE0/",$str))
 {
 echo $red."[+] Found Beast Trojan Horse 2.6 \n";
 $risk = $risk + 10;
-}
-else
-{
-echo $green."[-] Clear no Beast Trojan Horse 2.6 \n";
-}
 }
 else
 {
@@ -215,9 +149,7 @@ echo $red."\n$name ".$white."Found Please Wait..\n\n";
 echo $cyan."File Type (apk) ..\n\n";
 $str=shell_exec("strings $name");
 //SpyNote 5.0
-if(preg_match("/abc_textfield_search_default_mtrl_alpha.9.pngPK/",$str))
-{
-if(preg_match("/##Widget.AppCompat.PopupMenu.Overflow/",$str))
+if(preg_match("/abc_textfield_search_default_mtrl_alpha.9.pngPK/",$str)&&preg_match("/##Widget.AppCompat.PopupMenu.Overflow/",$str))
 {
 echo $red."[+] Found SpyNote ! \n";
 $risk = $risk + 10;
@@ -226,32 +158,12 @@ else
 {
 echo $green."[-] Clear no SpyNote \n";
 }
-}
-else
-{
-echo $green."[-] Clear no SpyNote \n";
-}
 
 //DroidJack
-if(preg_match("/zP_1cXD/",$str))
-{
-if(preg_match("/surface_camera/",$str))
-{
-if(preg_match("/cameraview.xml/",$str))
+if(preg_match("/zP_1cXD/",$str)&&preg_match("/surface_camera/",$str)&&preg_match("/cameraview.xml/",$str))
 {
 echo $red."[+] Found DroidJack \n";
 $risk = $risk + 10;
-}
-else
-{
-echo $green."[-] Clear no DroidJack \n";
-}
-}
-else
-{
-echo $green."[-] Clear no DroidJack \n";
-}
-
 }
 else
 {
